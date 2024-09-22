ede9fe2c6529c117802a0d1d6e198fd390e7a14f
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2024-09-22 20:15:34 +0530

    added scripts

--- db/db.js
+++ db/db.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 const Connection = async (url) => {
-	const URL = url;
 	try {
-		await mongoose.connect(URL, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-		});
+		await mongoose.connect(url);
 		console.log("Database connected successfully...");
 	} catch (error) {
 		console.log("Error while connecting with the database ", error);
